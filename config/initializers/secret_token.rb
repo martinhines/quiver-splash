7cd70bd41de9eab37063560bef8c3a357e7d2f88
Marty Hines <martin.hines@mminc.co>
2011-08-16 11:16:24 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuiverSplash::Application.config.secret_token = 'f138612cb271c71db09a73a14ca4e73f27da176af83afe2fa69899589ea2b37b6fe5951fe05c6ccd681d1fb79e474860cd615b1ffbeaf620da670e7936cf7e8b'
